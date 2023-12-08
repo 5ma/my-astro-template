e55addbe840282dd50f871fbaa46915017ac8a65
komakine-anna <anna.komakine@relic.co.jp>
2023-12-08 12:03:37 +0900

    💄 baseとなるscssファイル追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  scopedStyleStrategy: 'where',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "sass:math";
+          @use '@/styles/global/' as *;
+          `
+        }
+      }
+    }
+  }
+});
